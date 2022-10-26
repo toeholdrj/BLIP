0e7433669cc8a0e46d3257043a195b81f01564f1
Keunwoo Choi <4292508+toeholdrj@users.noreply.github.com>
2022-10-26 00:01:33 -0400

    to relative import

--- models/blip.py
+++ models/blip.py
@@ -8,8 +8,8 @@
 import warnings
 warnings.filterwarnings("ignore")
 
-from models.vit import VisionTransformer, interpolate_pos_embed
-from models.med import BertConfig, BertModel, BertLMHeadModel
+from .vit import VisionTransformer, interpolate_pos_embed
+from .med import BertConfig, BertModel, BertLMHeadModel
 from transformers import BertTokenizer
 
 import torch
